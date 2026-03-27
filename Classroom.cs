47bc99075f56d3ab7e365acdd282d1d4b1fa5128
dheim123 <dheim@midland.k12.ia.us>
2026-03-27 12:19:35 -0500

    Add CreateTeam method to Classroom class
    Implemented CreateTeam method to return a dictionary of team members.

--- Classroom.cs
+++ Classroom.cs
@@ -1,8 +1,14 @@
-﻿namespace knightmoves;
+namespace knightmoves;
 
-public class Classroom{
-
-    public Dictionary<int, string> CreateTeam(string first, string second, string third){
-        return null;
+public class Classroom
+{
+    public Dictionary<int, string> CreateTeam(string first, string second, string third)
+    {
+        return new Dictionary<int, string>
+        {
+            { 1, first },
+            { 2, second },
+            { 3, third }
+        };
     }
 }
